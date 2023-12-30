fa230df560dd4d077adcccd1e51461961faffec0
JonyPlo <joonyyplo@gmail.com>
2023-12-30 04:28:58 -0300

    feat: :sparkles: add initial configurations
    initial configurations added

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,8 @@
+// Snippet para crear el componente exportado por defecto es "prc"
+export default function CounterPage() {
+  return (
+    <div>
+      <h1>Hello Page Counter</h1>
+    </div>
+  );
+}
