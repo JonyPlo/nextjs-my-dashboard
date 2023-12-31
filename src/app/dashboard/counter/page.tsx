b0cadc226e2c5b996fc8012c487b978271351ddf
JonyPlo <joonyyplo@gmail.com>
2023-12-31 02:31:22 -0300

    feat(cart): :sparkles: add counter cart
    counter to manage stocks in the cart added

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,17 @@
+import { CartCounter } from '@/app/shopping-cart'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Shopping Cart',
+  description: 'A simple counter',
+}
+
 // Snippet para crear el componente exportado por defecto es "prc"
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Hello Page Counter</h1>
+    <div className='flex flex-col items-center justify-center w-full h-full'>
+      <span>Productos en el carrito</span>
+      <CartCounter value={20} />
     </div>
   )
 }
