894e57f8fec249e5a8a0ee08cbfa01f0f014150d
JonyPlo <joonyyplo@gmail.com>
2024-01-02 04:29:05 -0300

    feat(pokemons): :sparkles: add pokemons in pokemons page

--- src/app/components/sidebar/SideBar.tsx
+++ src/app/components/sidebar/SideBar.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5'
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoLogoReact } from 'react-icons/io5'
 import { SideBarMenuItem } from '..'
 
 const menuItems = [
@@ -15,6 +15,12 @@ const menuItems = [
     title: 'Counter',
     subTitle: 'Counter Client Side',
   },
+  {
+    path: '/dashboard/pokemons',
+    icon: <IoFootball size={40} />,
+    title: 'Pokemons',
+    subTitle: 'Static Generation',
+  },
 ]
 
 export const SideBar = () => {
@@ -22,7 +28,7 @@ export const SideBar = () => {
     <div
       id='menu'
       style={{ width: '400px' }}
-      className='bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll'
+      className='bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll'
     >
       <div className='flex justify-center'>
         <IoLogoReact className='m-2 size-10 my-5' />
